815e60dbd8a68d8c611501d8fdfd0ae5ce207abf
Yspyridis <io.spyridis@gmail.com>
2023-09-15 20:29:11 +0300

    add button for unit

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const calculateBtn = document.getElementById("calculateBtn");
+    const unitBtn = document.getElementById("unitBtn");
+    let isConsumptionPer100Km = true; // Default unit is L/100km
+
+    unitBtn.addEventListener("click", function () {
+        isConsumptionPer100Km = !isConsumptionPer100Km;
+        unitBtn.textContent = isConsumptionPer100Km ? "L/100km" : "km/L";
+    });
 
     calculateBtn.addEventListener("click", function () {
         const totalKm = parseFloat(document.getElementById("totalKm").value);
@@ -7,8 +14,13 @@ document.addEventListener("DOMContentLoaded", function () {
         const pricePerLiter = parseFloat(document.getElementById("pricePerLiter").value);
 
         if (!isNaN(totalKm) && !isNaN(consumption) && !isNaN(pricePerLiter)) {
-            const totalCost = (totalKm / 100) * consumption * pricePerLiter;
-            document.getElementById("totalCost").textContent = totalCost.toFixed(2);
+            if (isConsumptionPer100Km) {
+                const totalCost = (totalKm / 100) * consumption * pricePerLiter;
+                document.getElementById("totalCost").textContent = totalCost.toFixed(2);
+            } else {
+                const totalCost = (totalKm / consumption) * pricePerLiter;
+                document.getElementById("totalCost").textContent = totalCost.toFixed(2);
+            }
         } else {
             alert("Please enter valid numbers for all fields.");
         }
